dd8cc2150bbc384d93f07028f34ea71fae9b145c
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2020-05-28 16:43:50 +0700

    lab09

---
+++ lab09/13_10/13_10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+class Department : IEnumerable
+{
+    string[] departmentNames = {"Marketing", "Finance", "Information Technology", "Human Resources"};
+    public IEnumerator GetEnumerator()
+    {
+        for (int i = 0; i < departmentNames.Length; i++)
+        {
+            yield return departmentNames[i];
+        }
+    }
+    static void Main(string[] args)
+    {
+        Department objDepartment = new Department();
+        Console.WriteLine("Department Names");
+        Console.WriteLine();
+        foreach (string str in objDepartment)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ lab09/13_8/13_8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class General<T, U>
+{
+    T _valOne;
+    U _valTwo;
+    public void AcceptValues(T item)
+    {
+        _valOne = item;
+    }
+    public void AcceptValues(U item)
+    {
+        _valTwo = item;
+    }
+    public void Display()
+    {
+        Console.Write(_valOne + "\t" +_valTwo);
+    }
+}
+class MethodOverload
+{
+    static void Main(string[] args)
+    {
+        General<int, string> objGenOne = new General<int, string>();
+        objGenOne.AcceptValues(10);
+        objGenOne.AcceptValues("Smith");
+        Console.WriteLine("ID\tName\tDesignation\tSalary");
+        objGenOne.Display();
+        General<string, float> objGenTwo = new General<string, float>();
+        objGenTwo.AcceptValues("Mechanic");
+        objGenTwo.AcceptValues(2500);
+        Console.Write("\t");
+        objGenTwo.Display();
+        Console.WriteLine();
+    }
+}
